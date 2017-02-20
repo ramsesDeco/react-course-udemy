291d85b61dac7d73ff33c1b1916b11a18bc74ea7
Ramses <ramses@collectivemedia.mx>
2017-02-20 13:59:33 -0600

    react create element / diferencia entre jsx

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,20 @@
 var Greeter = React.createClass({
-	render: function() {
-		return (
+	render: function () {
+		return React.createElement(
+			"h1",
+			null,
+			"Hello React.createElement!"
+		)
+		/*return (
 			<div>
 				<h1>Hello Reactadd!</h1>
 				<p>this is from the component</p>
 			</div>
-		);
+		);*/
 	}
 });
 
 ReactDOM.render(
-	<Greeter/>,
+	<Greeter />,
 	document.getElementById('app')
 );
