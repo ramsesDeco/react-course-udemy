bd8ab11cc6a717846d2279951a54b6bb651fe4de
Ramses <ramses@collectivemedia.mx>
2017-02-22 09:02:34 -0600

    data flow simulated

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -1,11 +1,22 @@
 var React = require('react');
 
 var WeatherForm = React.createClass({
+	onFormSubmit: function (event) {
+		event.preventDefault();
+
+		var location = this.refs.location.value;
+
+		if (location.length > 0) {
+			this.refs.location.value = '';
+			this.props.onSearch(location);
+		}
+
+	},
 	render: function () {
 		return (
 			<div>
-				<form>
-					<input type="text"/>
+				<form onSubmit={this.onFormSubmit}>
+					<input type="text" ref="location" />
 					<button type="submit">Get Weather</button>
 				</form>
 			</div>
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,9 +2,10 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
 	render: function () {
+		var {location, temp} = this.props;
 		return (
 			<div>
-				<p>It's 40 in Philadelphia.</p>
+				<p>It's {temp} in {location}.</p>
 			</div>
 		);
 	}
