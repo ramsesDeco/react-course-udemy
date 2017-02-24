05ed543dd0f5ce1e705ff22075c8b868793ff9d6
Ramses <ramses@collectivemedia.mx>
2017-02-24 12:41:20 -0600

    init commit from boilerplate react app

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hasHistory, browserHistory } = require('react-router');
+var Main = require('Main');
+
+// Load foundation
+require('style!css!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}></Route>
+	</Router>,
+	document.getElementById('app')
+);
