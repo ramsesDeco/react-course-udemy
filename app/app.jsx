d0c76b947f89a64d9eaee6b38ede34c2345d162d
Ramses Yael De La Peña Coronado <ramses.deco@gmail.com>
2017-03-03 14:06:07 -0600

    integrate fundation vars to webpack
    sass-loader from v6 to v4

--- app/app.jsx
+++ app/app.jsx
@@ -7,7 +7,6 @@ var Timer = require('Timer');
 var Countdown = require('Countdown');
 
 // Load foundation
-require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
 // App css
