c13e3055eeb344455f5e03c246736b357fb7bada
Ramses Yael De La Peña Coronado <ramses.deco@gmail.com>
2017-03-24 11:51:18 -0600

    fixing the tests

--- app/tests/api/TodoApi.test.jsx
+++ app/tests/api/TodoApi.test.jsx
@@ -60,4 +60,50 @@ describe('TodoApi', () => {
 
     });
 
+
+    describe('filterTodos', () => {
+        var todos = [
+            {
+                id: 1,
+                text: 'Some text here',
+                completed: true
+            },
+            {
+                id: 2,
+                text: 'Other text here',
+                completed: false
+            },
+            {
+                id: 3,
+                text: 'Some text here',
+                completed: true
+            }
+        ];
+
+        it('should return all items if showCompleted is true', () => {
+            var filterTodos = TodoApi.filterTodos(todos, true, '');
+            expect(filterTodos.length).toBe(3);
+        });
+
+        it('should return non-completed todos when showCompleted is false', () => {
+            var filterTodos = TodoApi.filterTodos(todos, false, '');
+            expect(filterTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filterTodos = TodoApi.filterTodos(todos, true, '');
+            expect(filterTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by searchText', () => {
+            var filterTodos = TodoApi.filterTodos(todos, true, 'some');
+            expect(filterTodos.length).toBe(2);
+        });
+
+        it('should return all todos if searchText is empty', () => {
+            var filterTodos = TodoApi.filterTodos(todos, true, '');
+            expect(filterTodos.length).toBe(3);
+        });
+    });
+
 });
